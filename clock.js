d2834973f8e1893fe876dba4840c0266ecd5bf3d
chica25 <patricia@patriciaco.com>
2021-03-07 13:52:52 -0500

    Edit colorArry and changecolor function

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@
 const dataSecond = document.querySelector('[data-second]');
 const dataMinute = document.querySelector('[data-minute]');
 const dataHour = document.querySelector('[data-hour]');
-let  i = 0;
+let colorArray = ["#71b8bd", "#6ebcbe", "#6cc1be", "#6bc5be", "#6ac9bd", "#6bcdbb", "#6dd1b9", "#71d5b6", "#75d8b2", "#7bdcae", "#82dfa9", "#8ae2a4", "#93e59f", "#9de899", "#a7ea93", "#b2ed8d", "#beef87", "#caf082", "#d6f27c", "#e3f377"]
 
 function setClock() {
     let currentDate = new Date()
@@ -18,11 +18,9 @@ function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
 
-
 function changeColor() {
-  let colorArray = ["black", "blue", "brown", "green"];
   let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-document.getElementById('bg-color').style.backgroundColor = randomColor;
+    document.getElementById('bg-color').style.backgroundColor = randomColor;
 }
 
 setInterval(function(){
