a467929eece21916d90190f2ee526f3b3d37f2bb
chica25 <patricia@patriciaco.com>
2021-03-07 02:32:45 -0500

    Edit styling and implement js code

--- clock.js
+++ clock.js
@@ -1,1 +1,16 @@
-const clock = document.querySelector('clock')
+setInterval(setClock, 1000)
+
+const hour = document.querySelector('[data-hour]')
+const minute = document.querySelector('[data-minute]')
+const second = document.querySelector('[data-second]')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentData.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentData.getHours()) / 12
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--roation', rotationRatio * 360)
+}
